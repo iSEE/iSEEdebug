88403baccf961399ddc7c3e8cbe6a0c7053edd6b
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-07-19 17:33:51 +0100

    Create script_03.R

---
+++ issues/iSEE-issue-580/script_03.R
@@ -0,0 +1,38 @@
+## Issue: https://github.com/iSEE/iSEE/issues/580
+## Comment: https://github.com/iSEE/iSEE/issues/580#issuecomment-1172580704
+## YouTube video: n/a
+## Script adapted by Kevin Rue-Albrecht
+
+#' In this example, we use the 10X PBMD data set of 68k cells.
+#' This sufficient to cause the issue reported:
+#' Use the search box in the RowDataTable panel while the heatmap is receiving
+#' a row selection from that panel and uses custom rows;
+#' The heatmap refreshes (without actually changing changing its contents)
+#' every time that the set of rows visible in the RowDataTable changes.
+#' That should not happen.
+
+library(iSEE)
+
+# Data ----
+
+library(TENxPBMCData)
+sce <- TENxPBMCData("pbmc68k")
+
+## Example 1 ----
+
+initial <- list(
+    RowDataTable(
+        PanelWidth = 6L,
+        Selected = "Gene_0001"
+        ),
+    ComplexHeatmapPlot(
+        PanelWidth = 6L,
+        DataBoxOpen = TRUE,
+        CustomRows = TRUE,
+        SelectionBoxOpen = TRUE,
+        RowSelectionSource = "RowDataTable1"
+        )
+)
+
+app <- iSEE(sce, initial = initial)
+shiny::runApp(app, launch.browser = TRUE)
